48f0a5ec605a4a4465c4bcaa2de981a927966e61
billboggs <bill.m.boggs@gmail.com>
2025-02-07 19:49:17 -0500

    fix: update tests to fix linter error

--- src/views/resume/__tests__/Resume.test.tsx
+++ src/views/resume/__tests__/Resume.test.tsx
@@ -1,5 +1,5 @@
 import {screen} from '@testing-library/react';
-import {describe, expect, it, test} from 'vitest';
+import {describe, expect, test} from 'vitest';
 
 import {renderRouter} from '@billboggs/tests/render';
 import Resume from '@billboggs/views/resume/Resume';
